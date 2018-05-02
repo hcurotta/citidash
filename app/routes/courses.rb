bf359809fc88333b43d2c99ffbcf0c6210804138
Harry Curotta <hcurotta@gmail.com>
2018-05-02 23:44:33 +0100

    add route maps to routes endpoint

--- app/routes/courses.rb
+++ app/routes/courses.rb
@@ -11,6 +11,7 @@ module CitiDash
           results.map do |result|
             {
               id: result[:route_id],
+              map_url: RouteMapGenerator.new(result[:route_id]).map_url,
               trip_count: result[:trip_count],
               last_trip_ended_at: result[:last_trip_ended_at],
               origin: {
--- app/services/route_map_generator.rb
+++ app/services/route_map_generator.rb
@@ -7,8 +7,8 @@ module CitiDash
       MAPBOX_API_TOKEN = ENV["MAPBOX_API_TOKEN"]
       MAPBOX_PIN_URL = "https://s3.amazonaws.com/dockdash/assets/images/mapbox_pin@2x.png"
 
-      def initialize(route)
-        @route = route
+      def initialize(route_id)
+        @route = Route.find(id: route_id)
         @origin = @route.origin
         @destination = @route.destination
       end
@@ -28,21 +28,25 @@ module CitiDash
         origin_coords = "#{@origin.lon},#{@origin.lat}"
         destination_coords = "#{@destination.lon},#{@destination.lat}"
 
-        path = directions["routes"][0]["geometry"]
-        path_color = "FFCC00"
-        path_width = 5
-        path_opacity = 1
-        pin_url = CGI.escape(MAPBOX_PIN_URL)
+        begin
+          path = directions["routes"][0]["geometry"]
+          path_color = "FFCC00"
+          path_width = 5
+          path_opacity = 1
+          pin_url = CGI.escape(MAPBOX_PIN_URL)
 
 
-        base_map_url = "https://api.mapbox.com/styles/v1/hcurotta/cjelfept5afak2smsuyqg6uxc/static/"
-        path_string = "path-#{path_width}+#{path_color}-#{path_opacity}(#{path}),"
-        origin_pin_string = "url-#{pin_url}(#{origin_coords}),"
-        destination_pin_string = "url-#{pin_url}(#{destination_coords})"
-        zoom_and_size = "/auto/400x400@2x"
-        query_string = "?access_token=#{MAPBOX_API_TOKEN}&attribution=false&logo=false"
+          base_map_url = "https://api.mapbox.com/styles/v1/hcurotta/cjelfept5afak2smsuyqg6uxc/static/"
+          path_string = "path-#{path_width}+#{path_color}-#{path_opacity}(#{path}),"
+          origin_pin_string = "url-#{pin_url}(#{origin_coords}),"
+          destination_pin_string = "url-#{pin_url}(#{destination_coords})"
+          zoom_and_size = "/auto/400x400@2x"
+          query_string = "?access_token=#{MAPBOX_API_TOKEN}&attribution=false&logo=false"
 
-        base_map_url + path_string + origin_pin_string + destination_pin_string + zoom_and_size + query_string
+          base_map_url + path_string + origin_pin_string + destination_pin_string + zoom_and_size + query_string
+        rescue
+          "placeholder"
+        end
       end
     end
   end
